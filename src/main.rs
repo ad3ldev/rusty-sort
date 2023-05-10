a1c11eaa9f712316b0bbf7fd7aa1a7c26298e10f
AbdelRahman Adel AbdelFatttah <56457765+ad3ldev@users.noreply.github.com>
2023-05-10 17:14:39 +0300

    merge sort parallel

--- src/main.rs
+++ src/main.rs
@@ -4,41 +4,31 @@ mod merge_sort;
 mod quick_sort;
 mod radix_sort;
 
-use crate::bubble_sort::parallel_bubble_sort;
-use crate::bubble_sort::serial_bubble_sort;
-use crate::merge_sort::serial_merge_sort;
-use crate::quick_sort::serial_quick_sort;
-use crate::radix_sort::serial_radix_sort;
+use crate::bubble_sort::*;
+use crate::merge_sort::*;
+use crate::quick_sort::*;
 use rand::prelude::*;
+use std::thread::*;
 use std::time::Instant;
 
-fn gen_rand_arr<const SIZE: usize>(rng: &mut ThreadRng) -> [u64; SIZE] {
-    let mut arr = [0; SIZE];
-    for x in &mut arr {
-        *x = rng.gen_range(0..SIZE as u64);
-    }
-    arr
-}
-
 fn main() {
-    let mut rng = thread_rng();
-    // let mut arr: [u64; 1_000_000] = gen_rand_arr(&mut rng);
-    let mut arr = [813, 123, 9, 0, 241, 420];
-    let start = Instant::now();
+    let logical_processors: usize = available_parallelism().unwrap().get();
+    const CAPACITY: usize = 1_000_000;
+    let mut arr: [u64; CAPACITY] = [0; CAPACITY];
+    // rand::thread_rng().fill(&mut arr[..]);
 
+    // let mut arr = [76, 921, 32, 0, 237];
+
+    let start = Instant::now();
     // serial_bubble_sort(&mut arr);
     // parallel_bubble_sort(&mut arr);
     // serial_quick_sort(&mut arr);
     // serial_merge_sort(&mut arr);
     // serial_radix_sort(&mut arr);
     // serial_merge_sort(&mut arr);
-
-    for i in 0..arr.len() {
-        print!("{}  ", arr[i]);
-    }
-    println!();
-
+    parallel_merge_sort(&mut arr, 2);
     let end = Instant::now();
+
     // for i in arr.iter() {
     //     println!("{}", i);
     // }
--- src/merge_sort.rs
+++ src/merge_sort.rs
@@ -1,3 +1,5 @@
+use std::thread;
+
 fn serial_merge(arr: &mut [u64], start: usize, mid: usize, end: usize) {
     let len1 = mid - start + 1;
     let len2 = end - mid;
@@ -44,5 +46,41 @@ pub fn serial_merge_sort(arr: &mut [u64]) {
     serial_merge_sort_helper(arr, 0, len - 1);
 }
 
-fn parallel_merge(arr: &mut [u64], start: usize, mid: usize, end: usize) {}
-pub fn parallel_merge_sort(arr: &mut [u64], left: usize, right: usize) {}
+pub fn parallel_merge_sort(arr: &mut [u64], threads_num: usize) {
+    let len = arr.len();
+    if len == 0 {
+        return;
+    }
+    if len <= threads_num {
+        serial_merge_sort(arr)
+    }
+    let chunk_size = (len as f64 / threads_num as f64).ceil() as usize;
+    let mut start = 0;
+    let mut end = chunk_size;
+    let mut children = vec![];
+    for i in 0..threads_num {
+        let mut chunk = arr[start..end].to_vec();
+        children.push(thread::spawn(move || {
+            serial_merge_sort(&mut chunk);
+            chunk
+        }));
+        start += chunk_size;
+        end += chunk_size;
+        if end > len {
+            end = len;
+        }
+    }
+    start = 0;
+    end = chunk_size;
+    for child in children {
+        let chunk = child.join().unwrap();
+        arr[start..end].copy_from_slice(&chunk[..]);
+        start += chunk_size;
+        end += chunk_size;
+        if end > len {
+            end = len;
+        }
+    }
+    let mut mid = len / 2;
+    serial_merge(arr, 0, mid, len - 1)
+}
