248c83110473fa29b51b33d1aae240f2166a96df
AbdelRahman Adel AbdelFatttah <56457765+ad3ldev@users.noreply.github.com>
2023-05-10 09:19:13 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ mod quick_sort;
 mod radix_sort;
 
 use crate::bubble_sort::parallel_bubble_sort;
-use crate::bubble_sort::parallel_bubble_sort;
 use crate::bubble_sort::serial_bubble_sort;
 use crate::merge_sort::serial_merge_sort;
 use crate::quick_sort::serial_quick_sort;
