118e17447b6adde253fa47808a1275cd3a38f8bf
AbdelRahman Adel AbdelFatttah <56457765+ad3ldev@users.noreply.github.com>
2023-05-10 20:48:58 +0300

    merge sort parallel

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,9 @@ fn main() {
     let logical_processors: usize = available_parallelism().unwrap().get();
     const CAPACITY: usize = 1_000_000;
     let mut arr: [u64; CAPACITY] = [0; CAPACITY];
-    // rand::thread_rng().fill(&mut arr[..]);
+    rand::thread_rng().fill(&mut arr[..]);
 
-    // let mut arr = [76, 921, 32, 0, 237];
+    // let mut arr = [76, 921, 32, 0, 237, 24, 132, 40, 1248, 21394, 4, 14];
 
     let start = Instant::now();
     // serial_bubble_sort(&mut arr);
@@ -26,10 +26,10 @@ fn main() {
     // serial_merge_sort(&mut arr);
     // serial_radix_sort(&mut arr);
     // serial_merge_sort(&mut arr);
-    parallel_merge_sort(&mut arr, 2);
+    parallel_merge_sort(&mut arr, 8);
     let end = Instant::now();
 
-    // for i in arr.iter() {
+    // for i in arr.iter()
     //     println!("{}", i);
     // }
     println!("Time elapsed: {:?}", end.duration_since(start));
