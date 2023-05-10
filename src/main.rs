2254c0390d97475b9593b2e0a5063db09cf0d956
AbdelRahman Adel AbdelFatttah <56457765+ad3ldev@users.noreply.github.com>
2023-05-10 09:14:14 +0300

    radix_sort

--- src/main.rs
+++ src/main.rs
@@ -7,26 +7,31 @@ mod radix_sort;
 use crate::bubble_sort::serial_bubble_sort;
 use crate::merge_sort::serial_merge_sort;
 use crate::quick_sort::serial_quick_sort;
+use crate::radix_sort::serial_radix_sort;
 use rand::prelude::*;
 use std::time::Instant;
 
 fn gen_rand_arr<const SIZE: usize>(rng: &mut ThreadRng) -> [u64; SIZE] {
     let mut arr = [0; SIZE];
     for x in &mut arr {
-        *x = rng.gen_range(u64::MIN..u64::MAX);
+        *x = rng.gen_range(0..SIZE as u64);
     }
     arr
 }
 
 fn main() {
     let mut rng = thread_rng();
-    let mut arr: [u64; 1_000_000] = gen_rand_arr(&mut rng);
-
+    // let mut arr: [u64; 1_000_000] = gen_rand_arr(&mut rng);
+    let mut arr = [813, 123, 9, 0, 241, 420];
     let start = Instant::now();
-    serial_bubble_sort(&mut arr);
+    // serial_bubble_sort(&mut arr);
     // serial_quick_sort(&mut arr);
     // serial_merge_sort(&mut arr);
+    serial_radix_sort(&mut arr);
     let end = Instant::now();
 
+    // for i in arr.iter() {
+    //     println!("{}", i);
+    // }
     println!("Time elapsed: {:?}", end.duration_since(start));
 }
