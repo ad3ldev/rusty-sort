1d05054e432e692d640f921824be7d64405e674b
MohamedMetwalli5 <mohamedmetwalli5@gmail.com>
2023-05-12 21:13:34 +0200

    QuickSort Fixed

--- src/quick_sort.rs
+++ src/quick_sort.rs
@@ -1,30 +1,3 @@
-// fn serial_quick_sort_helper_old(arr: &mut [u64], left: usize, right: usize) {
-//     let mut i = left;
-//     let mut j = right;
-//     let pivot = arr[(left + right) / 2];
-//     while i <= j {
-//         while arr[i] < pivot {
-//             i += 1;
-//         }
-//         while arr[j] > pivot {
-//             j -= 1;
-//         }
-//         if i <= j {
-//             let temp = arr[i];
-//             arr[i] = arr[j];
-//             arr[j] = temp;
-//             i += 1;
-//             j -= 1;
-//         }
-//     }
-//     if left < j {
-//         serial_quick_sort_helper(arr, left, j);
-//     }
-//     if i < right {
-//         serial_quick_sort_helper(arr, i, right);
-//     }
-// }
-
 fn partition(arr: &mut [u64], low: isize, high: isize) -> isize {
     let pivot = arr[high as usize];
     let mut i = ((low) as isize) - 1;
