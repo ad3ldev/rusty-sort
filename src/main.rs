fa735e8ac3a9f933d1840cf408694a060fa98a50
AbdelRahman Adel AbdelFatttah <56457765+ad3ldev@users.noreply.github.com>
2023-05-13 19:31:30 +0300

    input

--- src/main.rs
+++ src/main.rs
@@ -48,24 +48,29 @@ fn serial_running(control: &mut [u64], arr: &mut [u64], serial_sort: fn(&mut [u6
 fn main() {
     // let processors: usize = availa  ble_parallelism().unwrap().get();
     let threads = vec![2, 4, 8, 16, 32];
-    let size = 500_000_000;
+    let mut size: usize;
+    let mut buffer = String::new();
+    std::io::stdin().read_line(&mut buffer);
+    println!("Input size of array: ");
+    size = buffer.trim().parse().unwrap();
 
-    let mut arr: Vec<u64> = vec![0; size];
-    rand::thread_rng().fill(&mut arr[..]);
-    let mut control = arr.clone();
-    let mut parallel = arr.clone();
+    let mut original: Vec<u64> = vec![0; size];
+    rand::thread_rng().fill(&mut original[..]);
+    let mut serial = original.clone();
+    let mut control = original.clone();
 
-    serial_running(&mut control, &mut arr, serial_merge_sort);
+    serial_running(&mut control, &mut serial, serial_merge_sort);
+    if !check_if_equal(&mut control, &mut serial) {
+        panic!("NOT EQUAL SERIAL");
+    }
     println!("###############################################");
     for thread in threads {
+        let mut parallel = original.clone();
         println!("Sizes: {}\tThreads: {}\n", size, thread);
         parallel_running(&mut parallel, thread, parallel_merge_sort);
-        println!("------------------------------------\n")
-    }
-    if !check_if_equal(&mut control, &mut arr) {
-        panic!("NOT EQUAL SERIAL");
-    }
-    if !check_if_equal(&mut control, &mut parallel) {
-        panic!("NOT EQUAL SERIAL");
+        println!("------------------------------------\n");
+        if !check_if_equal(&mut control, &mut parallel) {
+            panic!("NOT EQUAL SERIAL");
+        }
     }
 }
