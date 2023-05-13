a6c1627e69014e053cb020bd874d752f29ab8178
AbdelRahman Adel AbdelFatttah <56457765+ad3ldev@users.noreply.github.com>
2023-05-13 18:52:23 +0300

    biggest run

--- src/main.rs
+++ src/main.rs
@@ -23,17 +23,15 @@ fn check_if_equal(control: &mut [u64], tested: &mut [u64]) -> bool {
     return true;
 }
 
-fn running_samples(
-    size: usize,
-    processors: usize,
-    parallel_sort: fn(&mut [u64], usize),
-    serial_sort: fn(&mut [u64]),
-) {
-    let mut arr: Vec<u64> = vec![0; size];
-    rand::thread_rng().fill(&mut arr[..]);
-    let mut control = arr.clone();
-    let mut parallel = arr.clone();
+fn parallel_running(parallel: &mut [u64], processors: usize, parallel_sort: fn(&mut [u64], usize)) {
+    // Parallel
+    let start = Instant::now();
+    parallel_sort(parallel, processors);
+    let end = Instant::now();
+    println!("Parallel:\t{:?}, {}", end.duration_since(start), processors);
+}
 
+fn serial_running(control: &mut [u64], arr: &mut [u64], serial_sort: fn(&mut [u64])) {
     // Control
     let mut start = Instant::now();
     control.sort();
@@ -42,31 +40,32 @@ fn running_samples(
 
     // Serial
     start = Instant::now();
-    serial_sort(&mut arr);
+    serial_sort(arr);
     end = Instant::now();
-    println!("Serialized:\t{:?}, 1", end.duration_since(start));
-
-    // Parallel
-    start = Instant::now();
-    parallel_sort(&mut parallel, processors);
-    end = Instant::now();
-    println!("Parallel:\t{:?}, {}", end.duration_since(start), processors);
-
-    if !check_if_equal(&mut control, &mut arr) {
-        panic!("NOT EQUAL SERIAL");
-    }
-    if !(check_if_equal(&mut control, &mut parallel)) {
-        panic!("NOT EQUAL PARALLEL");
-    }
+    println!("Serialized\t{:?}, 1", end.duration_since(start));
 }
 
 fn main() {
-    // let processors: usize = available_parallelism().unwrap().get();
+    // let processors: usize = availa  ble_parallelism().unwrap().get();
     let threads = vec![2, 4, 8, 16, 32];
-    let size = 100_000_000;
+    let size = 500_000_000;
+
+    let mut arr: Vec<u64> = vec![0; size];
+    rand::thread_rng().fill(&mut arr[..]);
+    let mut control = arr.clone();
+    let mut parallel = arr.clone();
+
+    serial_running(&mut control, &mut arr, serial_merge_sort);
+    println!("###############################################");
     for thread in threads {
         println!("Sizes: {}\tThreads: {}\n", size, thread);
-        running_samples(size, thread, parallel_merge_sort, serial_merge_sort);
+        parallel_running(&mut parallel, thread, parallel_merge_sort);
         println!("------------------------------------\n")
+    }
+    if !check_if_equal(&mut control, &mut arr) {
+        panic!("NOT EQUAL SERIAL");
+    }
+    if !check_if_equal(&mut control, &mut parallel) {
+        panic!("NOT EQUAL SERIAL");
     }
 }
